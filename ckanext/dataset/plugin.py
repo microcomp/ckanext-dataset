c489b96287b5333d27e3848d644d2c6c9f6303ea
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-09-02 14:42:46 +0200

    new resource attrs - transformed, transformation_executor

--- ckanext/dataset/plugin.py
+++ ckanext/dataset/plugin.py
@@ -449,11 +449,11 @@ def retrieve_geojson(data_extras):
 def get_users(data):
     users = tk.get_action('user_list')(data_dict={})
     return users, tk.c.user
-def get_name(login):
-    user_obj = model.Session.query(model.User).filter(model.User.name == login).first()
+def get_name(user_reference):
+    user_obj = model.User.get(user_reference)
     if user_obj:
         return user_obj.fullname
-    return login
+    return user_reference
 
 
 
@@ -463,15 +463,6 @@ class ExtendedDatasetPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
     plugins.implements(plugins.ITemplateHelpers, inherit=False)
     plugins.implements(plugins.IActions, inherit=False)
     plugins.implements(plugins.IResourceController, inherit=True)
-    
-    num_times_new_template_called = 0
-    num_times_read_template_called = 0
-    num_times_edit_template_called = 0
-    num_times_search_template_called = 0
-    num_times_history_template_called = 0
-    num_times_package_form_called = 0
-    num_times_check_data_dict_called = 0
-    num_times_setup_template_variables_called = 0
 
     def before_show(self, resource_dict):
         try:
@@ -539,7 +530,9 @@ class ExtendedDatasetPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
                         'schema': [tk.get_validator('ignore_missing'), validator_url],
                         'data_correctness' : [validator_data_correctness, unicode],
                         'data_correctness_description' : [validator_data_correctness_descr, unicode],
-                        'status' : [tk.get_validator('ignore_missing'), validator_status]
+                        'status' : [tk.get_validator('ignore_missing'), validator_status],
+                        'transformed' : [tk.get_validator('ignore_missing'), tk.get_validator('boolean_validator')],
+                        'transformation_executor' : [tk.get_validator('ignore_missing'), unicode]
             })
         
         return schema
@@ -569,53 +562,39 @@ class ExtendedDatasetPlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
                         'active_from' : [validator_date, unicode],
                         'active_to' : [validator_date, unicode],
                         'validity_description' : [validator_validity_descr, unicode],
-                        #'periodicity' : [tk.get_converter('convert_from_tags')('periodicities'),validator_periodicity],
                         'periodicity' : [validator_periodicity, unicode],
                         'periodicity_description' : [validator_periodicity_descr, unicode],
                         'schema': [tk.get_validator('ignore_missing'), validator_url],
                         'data_correctness' : [validator_data_correctness, unicode],
                         'data_correctness_description' : [validator_data_correctness_descr, unicode],
-                        'status' : [tk.get_validator('ignore_missing'), validator_status]
+                        'status' : [tk.get_validator('ignore_missing'), validator_status],
+                        'transformed' : [tk.get_validator('ignore_missing'), tk.get_validator('boolean_validator')],
+                        'transformation_executor' : [tk.get_validator('ignore_missing'), unicode]
             })
         
         return schema
 
-    # These methods just record how many times they're called, for testing
-    # purposes.
-    # TODO: It might be better to test that custom templates returned by
-    # these methods are actually used, not just that the methods get
-    # called.
-
     def setup_template_variables(self, context, data_dict):
-        ExtendedDatasetPlugin.num_times_setup_template_variables_called += 1
         return super(ExtendedDatasetPlugin, self).setup_template_variables(
                 context, data_dict)
 
     def new_template(self):
-        ExtendedDatasetPlugin.num_times_new_template_called += 1
         return super(ExtendedDatasetPlugin, self).new_template()
 
     def read_template(self):
-        ExtendedDatasetPlugin.num_times_read_template_called += 1
         return super(ExtendedDatasetPlugin, self).read_template()
 
     def edit_template(self):
-        ExtendedDatasetPlugin.num_times_edit_template_called += 1
         return super(ExtendedDatasetPlugin, self).edit_template()
 
     def search_template(self):
-        ExtendedDatasetPlugin.num_times_search_template_called += 1
         return super(ExtendedDatasetPlugin, self).search_template()
 
     def history_template(self):
-        ExtendedDatasetPlugin.num_times_history_template_called += 1
         return super(ExtendedDatasetPlugin, self).history_template()
 
     def package_form(self):
-        ExtendedDatasetPlugin.num_times_package_form_called += 1
         return super(ExtendedDatasetPlugin, self).package_form()
 
-    # check_data_dict() is deprecated, this method is only here to test that
-    # legacy support for the deprecated method works.
     def check_data_dict(self, data_dict, schema=None):
-        ExtendedDatasetPlugin.num_times_check_data_dict_called += 1
+        pass
